99832d91eab64de42eaf14bc979c9907b105e05e
tylorr <tylor@fastmail.com>
2016-06-04 22:47:16 -0700

    fix map, add filter and every

--- functional.lua
+++ functional.lua
@@ -1,12 +1,22 @@
 local functional = {}
 
-function functional.map(func, tbl)
+function functional.map(tbl, func)
   local result = {}
   for k,v in pairs(tbl) do
     result[k] = func(v, k, tbl)
   end
   return result
 end
+
+ function functional.filter(tbl, func)
+  local result = {}
+  for k,v in pairs(tbl) do
+    if func(v, k) then
+      result[k] = v
+    end
+  end
+  return result
+ end
 
 function functional.flatten(array, shallow)
   local result = {}
@@ -22,6 +32,15 @@ function functional.flatten(array, shallow)
   end
   return result
 end
+
+function functional.every(tbl, fn)
+  for _,value in pairs(tbl) do
+    if not fn(value) then return false end
+  end
+  return true
+end
+
+function functional.identity(x) return x end
 
 function functional.bind(func, arg1)
   return function(...) func(arg1, ...) end
