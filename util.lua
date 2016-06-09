3428bee014fe84dbd793fdcac5a32bf44d4b5845
tylorr <tylor@fastmail.com>
2016-06-08 21:57:39 -0700

    add util.switch

--- util.lua
+++ util.lua
@@ -8,4 +8,12 @@ function util.unpack(t)
   return unpack(t, 1, t.n)
 end
 
+function util.switch(cases, casevar, ...)
+  local func = cases[casevar] or cases.default
+  if func then
+    assert(type(func) == 'function', 'caseof only excepts table of functions')
+    return func(casevar, ...)
+  end
+end
+
 return util
