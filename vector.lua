a0021215acbe9075a522222e3c201388a0da5618
tylorr <tylor@fastmail.com>
2016-07-21 21:58:55 -0700

    convert vector to vec2 instead of vec3

--- vector.lua
+++ vector.lua
@@ -2,50 +2,49 @@ local class = require 'middleclass'
 
 local Vector = class('Vector')
 
-function Vector:initialize(x, y, z)
+function Vector:initialize(x, y)
   self.x = x or 0
   self.y = y or 0
-  self.z = z or 0
 end
 
 function Vector.__add(v1, v2)
-  return Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z)
+  return Vector(v1.x + v2.x, v1.y + v2.y)
 end
 
 function Vector.__sub(v1, v2)
-  return Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z)
+  return Vector(v1.x - v2.x, v1.y - v2.y)
 end
 
 function Vector:__unm()
-  return Vector(-self.x, -self.y, -self.z)
+  return Vector(-self.x, -self.y)
 end
 
 function Vector.__mul(a, b)
   if type(a) == 'number' then
-    return Vector(b.x * a, b.y * a, b.z * a)
+    return Vector(b.x * a, b.y * a)
   elseif type(b) == 'number' then
-    return Vector(a.x * b, a.y * b, a.z * b)
+    return Vector(a.x * b, a.y * b)
   else
-    return Vector(a.x * b.x, a.y * b.y, a.z * b.z)
+    return Vector(a.x * b.x, a.y * b.y)
   end
 end
 
 function Vector.__div(a, b)
   if type(a) == 'number' then
-    return Vector(a/ b.x , a / b.y, a / b.z)
+    return Vector(a/ b.x , a / b.y)
   elseif type(b) == 'number' then
-    return Vector(a.x / b, a.y / b, a.z / b)
+    return Vector(a.x / b, a.y / b)
   else
-    return Vector(a.x / b.x, a.y / b.y, a.z / b.z)
+    return Vector(a.x / b.x, a.y / b.y)
   end
 end
 
 function Vector.__eq(v1, v2)
-  return v1.x == v2.x and v1.y == v2.y and v1.z == v2.z
+  return v1.x == v2.x and v1.y == v2.y
 end
 
 function Vector:__tostring()
-  return '(' .. self.x .. ', ' .. self.y .. ', ' .. self.z .. ')'
+  return '(' .. self.x .. ', ' .. self.y .. ')'
 end
 
 function Vector:__len()
@@ -60,10 +59,6 @@ function Vector:xy()
   return self.x, self.y
 end
 
-function Vector:xyz()
-  return self.x, self.y, self.z
-end
-
 function Vector:sqrMagnitude()
   return Vector.dot(self, self)
 end
@@ -76,16 +71,25 @@ function Vector:normalized()
   return self / self:magnitude()
 end
 
-function Vector.static.dot(v1, v2)
-  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
-end
-
-function Vector.static.cross(v1, v2)
+function Vector:rotated(phi)
+  local c = math.cos(phi)
+  local s = math.sin(phi)
   return Vector(
-    v1.y * v2.z - v1.z * v2.y,
-    v1.z * v2.x - v1.x * v2.z,
-    v1.x * v2.y - v1.y * v2.x
+    c * self.x - s * self.y,
+    s * self.x + c * self.y
   )
 end
+
+function Vector.static.dot(v1, v2)
+  return v1.x * v2.x + v1.y * v2.y
+end
+
+-- function Vector.static.cross(v1, v2)
+--   return Vector(
+--     v1.y * v2.z - v1.z * v2.y,
+--     v1.z * v2.x - v1.x * v2.z,
+--     v1.x * v2.y - v1.y * v2.x
+--   )
+-- end
 
 return Vector
